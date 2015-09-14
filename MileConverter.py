c8e5c42c422611f8f0f29c12e6436c4333227182
cynthiacarter1 <cynthia.carter@myemail.indwes.edu>
2015-09-14 19:32:33 +0000

    CarterWeekTwo.py

---
+++ MileConverter.py
@@ -0,0 +1,12 @@
+__author__ = 'Cynthia Carter'
+# CS 125 Fall 2015
+# MileConvertor.py
+# a program that coverts Killimeters to miles
+
+def main () :
+
+    K = eval (input("Enter Killometers"))
+    M = K * .62
+    print ("You have traveled", M, "Miles")
+
+main()
---
+++ tempconverter.py
@@ -0,0 +1,12 @@
+__author__ = 'Cynthia Carter'
+# CS 125 Fall 2015
+# Tempconverter.py
+# a program that coverts Temperature from F to C 
+
+def main () :
+
+    F = eval (input("What is the Tempature in F?"))
+    C = (F-32) * 5/9
+    print ("The temperature is", C, "Degrees F")
+
+main()
