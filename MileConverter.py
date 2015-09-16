9cfc900d54175d0141e6c1fe62bbd4fb150219f4
cynthiacarter <cynthia.carter@myemail.indwes.edu>
2015-09-16 10:47:26 -0700

    Changes made
    added range

--- MileConverter.py
+++ MileConverter.py
@@ -1,13 +1,13 @@
 __author__ = 'Cynthia Carter'
 # CS 125 Fall 2015
-# Tempconverter.py
-# a program that coverts Temperature from F to C 
+# MileConvertor.py
+# a program that coverts Killimeters to miles
 
 def main () :
 
-    F = eval (input("What is the Tempature in F?"))
-    C = (F-32) * 5/9
-    print ("The temperature is", C, "Degrees F")
+    K = eval (input("Enter Killometers"))
+    M = K * .62
+    print ("You have traveled", M, "Miles")
     range (0, 101, 10)
 
 main()
